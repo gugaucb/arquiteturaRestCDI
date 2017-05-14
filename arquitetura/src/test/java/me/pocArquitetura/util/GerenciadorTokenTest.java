1de3f96f67a0a10574f8431d6c84ec5f5a0fb807
wallacerodrigon <wallacerodrigon@gmail.com>
2017-05-14 12:23:53 -0300

    Gerenciador de Token JWT.

---
+++ arquitetura/src/test/java/me/pocArquitetura/util/GerenciadorTokenTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package me.pocArquitetura.util;
+
+import junit.framework.TestCase;
+
+/**
+ * @author wallace
+ *
+ */
+public class GerenciadorTokenTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * Test method for {@link me.pocArquitetura.util.GerenciadorToken#novaInstancia()}.
+	 */
+	public void testNovaInstancia() {
+	}
+
+	/**
+	 * Test method for {@link me.pocArquitetura.util.GerenciadorToken#useEssaFraseSecreta(java.lang.String)}.
+	 */
+	public void testUseEssaFraseSecreta() {
+	}
+
+	/**
+	 * Test method for {@link me.pocArquitetura.util.GerenciadorToken#comEssesDados(java.lang.String[])}.
+	 */
+	public void testComEssesDados() {
+	}
+
+	/**
+	 * Test method for {@link me.pocArquitetura.util.GerenciadorToken#expireDaquiA(int)}.
+	 */
+	public void testExpireDaquiA() {
+	}
+
+	/**
+	 * Test method for {@link me.pocArquitetura.util.GerenciadorToken#gereToken()}.
+	 */
+	public void testGereToken() {
+	}
+
+	/**
+	 * Test method for {@link me.pocArquitetura.util.GerenciadorToken#esseTokenEValido(java.lang.String)}.
+	 */
+	public void testEsseTokenEValido() {
+	}
+
+	/**
+	 * Test method for {@link me.pocArquitetura.util.GerenciadorToken#devolvaMeOsDadosDoUsuario(java.lang.String)}.
+	 */
+	public void testDevolvaMeOsDadosDoUsuario() {
+	}
+
+}
